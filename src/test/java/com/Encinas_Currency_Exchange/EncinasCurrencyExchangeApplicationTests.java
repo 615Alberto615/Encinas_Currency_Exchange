10c8706b61916f6cfc4ae950cefa7d5c88338a34
615Alberto615 <a.encinas115@gmail.com>
2023-08-16 21:01:37 -0400

    first commit

---
+++ src/test/java/com/Encinas_Currency_Exchange/EncinasCurrencyExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Encinas_Currency_Exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncinasCurrencyExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
